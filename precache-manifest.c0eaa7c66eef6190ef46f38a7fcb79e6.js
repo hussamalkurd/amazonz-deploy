d4aad79a9edcc26275a488f5cc9e329582160fa8
hussam kurd <hussam-kurd@hotmail.com>
2020-09-20 03:55:47 +0300

    Updates

---
+++ precache-manifest.c0eaa7c66eef6190ef46f38a7fcb79e6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9119ff3fa3c6fbb3a6fe49e1f82b0f4c",
+    "url": "/hussamalkurd/amazonz-deploy/index.html"
+  },
+  {
+    "revision": "6209cc4263a691deb40a",
+    "url": "/hussamalkurd/amazonz-deploy/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "790aabeb908fb0f5356d",
+    "url": "/hussamalkurd/amazonz-deploy/static/css/main.36dca0ae.chunk.css"
+  },
+  {
+    "revision": "6209cc4263a691deb40a",
+    "url": "/hussamalkurd/amazonz-deploy/static/js/2.3e79a2e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hussamalkurd/amazonz-deploy/static/js/2.3e79a2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "790aabeb908fb0f5356d",
+    "url": "/hussamalkurd/amazonz-deploy/static/js/main.ae2680ef.chunk.js"
+  },
+  {
+    "revision": "5badf58730439afa160b",
+    "url": "/hussamalkurd/amazonz-deploy/static/js/runtime-main.fd2c6b28.js"
+  },
+  {
+    "revision": "bf79b6757027c409ee6c6107ee2074ce",
+    "url": "/hussamalkurd/amazonz-deploy/static/media/devgrub.bf79b675.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/hussamalkurd/amazonz-deploy/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "ae592d581444420b5c3e702d8840253b",
+    "url": "/hussamalkurd/amazonz-deploy/static/media/hussaa.ae592d58.png"
+  }
+]);
